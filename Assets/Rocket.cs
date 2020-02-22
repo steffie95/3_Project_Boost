9ae16a83f1346c3b8ecbfc08e570bddf8a5b1c89
steffie95 <60145379+steffie95@users.noreply.github.com>
2020-02-22 17:58:21 -0500

    Commit_05
    Added basic rocket flight.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.D) && ! Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Right");
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Left");
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+}
