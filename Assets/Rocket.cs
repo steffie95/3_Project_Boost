2c8cec0b962a0d7666eef6ad040c6e0b9d7536c2
steffie95 <60145379+steffie95@users.noreply.github.com>
2020-02-23 00:25:36 -0500

    Commit_07
    Added basic collision detection. Added obstacles. Created basic thrust algorithm. Created rocket rotation algorithm. Modified rocket appearance.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,6 +4,9 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource rocketThrust;
+   [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,32 +17,49 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotate();
+        Thrust();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly": print("OK"); break;
+            case "Fuel": print("Fuel"); break;
+            default: print("RIP"); break;
+        }
     }
 
 
-    private void ProcessInput()
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        
+
+        rigidBody.freezeRotation = false;
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!rocketThrust.isPlaying) rocketThrust.Play();
-
         }
 
         else rocketThrust.Stop();
-
-        if (Input.GetKey(KeyCode.D) && ! Input.GetKey(KeyCode.A))
-        {
-            print("Rotating Right");
-            transform.Rotate(Vector3.forward);
-        }
-
-        else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
-        {
-            print("Rotating Left");
-            transform.Rotate(-Vector3.forward);
-        }
     }
-
 }
